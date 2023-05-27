371df83015b2345662fd7d09b8532d58701f7bab
Maruf Ahmed <mk6116055@gmail.com>
2023-05-27 11:39:48 +0600

    initial commit

--- lib/pages/content_model.dart
+++ lib/pages/content_model.dart
@@ -8,14 +8,14 @@ class UnbordingContent {
 List<UnbordingContent> contents = [
   UnbordingContent(
     title: 'Book your ticket from home!',
-    image: 'assets/bus1.jpg',
+    image: 'assets/book.png',
   ),
   UnbordingContent(
     title: 'You can choose your preferable seats',
-    image: 'assets/seat.jpg',
+    image: 'assets/seat.png',
   ),
   UnbordingContent(
-    title: 'You are just few clicks away',
-    image: 'assets/bus2.png',
+    title: 'Welcome to Ticket Booking App',
+    image: 'assets/welcome.png',
   ),
 ];
